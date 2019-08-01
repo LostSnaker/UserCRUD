bcefe0c3577968bee80e7417614218c5ecc1e8e7
LostSnaker <lostsnaker@gmail.com>
2019-08-01 18:54:47 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -2,13 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+const user_controller = require('./controllers/user_controller');
+
 app.use(bodyParser.json());
 
 var user = require('./routes/user_routes');
 
 app.use('/users', user);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+    user_controller.checkIndex();
 });
 
--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -4,91 +4,55 @@ var client = new elasticsearch.Client({
     host: 'localhost:9200'
 });
 
-function checkInput(input) {
-    result = !(input.name) || !(input.surname) || !(input.birthdayDate) || !(input.phone)
-        || !(input.email);
-    return result;
-}
 
-
-function handleErrors(statusCode){
-    var errorMessages = new Map([
-        [400,"Error: Bad Request"],
-        [404,"Error: Not Found"],
-        [409,"Error: Conflict"],
-        [500,"Internal Server Error"]]);
-    return errorMessages.get(statusCode);
+function validateUser(user, phone, email){
+    if (user.phone === phone) {
+        return "This phone is already in use!";
+    } else if (user.email === email) {
+        return "This email is already in use!";
+    }
 }
 
 exports.userCreate = function (req, res) {
 
     let {name, surname, birthdayDate, phone, email, dateOfChange} = req.body;
 
-    result = checkInput(req.body)
-
     client.search({
         index: 'test-users',
         body:{
             "query" : {
-                "constant_score" : {
-                    "filter" : {
-                        "term" : {
-                            phone
-                        }
-                    }
+                "bool" : {
+                    "should" : [
+                        {"match_phrase" : {
+                                email
+                            }},
+                        {"term" : {
+                                phone
+                            }}
+                    ]
                 }
-            },  "size": 0
+            },  "size": 1
         }
-    },function(error, resp, status) {
-
-        if (resp.hits.total.value == 0) {
-
-            client.search({
-                index: 'test-users',
-                body:{"query" : {
-                        "constant_score" : {
-                            "filter" : {
-                                "match_phrase" : {
-                                    email
-                                }
-                            }
-                        }
-                    },  "size": 0
-                }
-            },function(error, resp, status) {
-
-                if (resp.hits.total.value == 0) {
-
-                    client.index({
-                        index: 'test-users',
-                        type: 'user',
-                        body: {
-                            name,
-                            surname,
-                            birthdayDate,
-                            phone,
-                            email,
-                            dateOfChange
-                        }
-                    },function (error, resp, status) {
-                        if (error) {
-                            res.send(handleErrors(status));
-                        }
-                        else {
-                            res.send("User was successfully created!");
-                        }
-
-                    })
-                }else{
-                    res.status(409).send("This email is already in use!");
-                }
-
-            });
+    }, function (error, resp, status) {
+        if (resp.hits.hits[0] !== undefined) {
+            res.status(409).send(validateUser(resp.hits.hits[0]._source, phone, email));
         } else {
-            res.status(409).send("This phone number is already in use!");
+            client.index({
+                index: 'test-users',
+                type: 'user',
+                body: {
+                    name,
+                    surname,
+                    birthdayDate,
+                    phone,
+                    email,
+                    dateOfChange
+                }
+            }, (error, resp, status) => {
+                res.send((error ? status + ": " + error.message : "User data was successfully added!"));
+            });
         }
     });
-
 };
 
 exports.userRead = function (req, res) {
@@ -98,9 +62,9 @@ exports.userRead = function (req, res) {
         index: 'test-users',
         type: 'user',
         id: entityId
-    },function(error, resp, status) {
+    }, (error, resp, status) => {
         if(error) {
-            res.send(handleErrors(status));
+            res.send(status + ": " + error.message);
         }
         else {
             res.json(resp._source);
@@ -125,9 +89,9 @@ exports.userUpdate = function (req, res) {
             email,
             dateOfChange
         }
-    },function(error, resp, status) {
+    }, (error, resp, status) => {
         if(error) {
-            res.send(handleErrors(status));
+            res.send(status + ": " + error.message);
         }
         else {
             res.send("User data was successfully updated!");
@@ -142,9 +106,9 @@ exports.userDelete = function (req, res) {
         index: 'test-users',
         id,
         type: 'user'
-    },function(error,resp,status) {
+    }, (error, resp, status) => {
         if(error) {
-            res.send(handleErrors(status));
+            res.send(status + ": " + error.message);
         }
         else {
             res.send("User data was successfully deleted!");
@@ -154,16 +118,17 @@ exports.userDelete = function (req, res) {
 
 exports.readAll = function (req, res) {
 
+
     client.search({
         index: 'test-users',
         body: {
             query: {
                 "match_all": {}
-            }
+            }, "size" : 100
         }
-    },function(error, resp, status) {
+    }, (error, resp, status) => {
         if(error) {
-            res.send(handleErrors(status));
+            res.send(status + ": " + error.message);
         }
         else {
             res.status = 200;
@@ -171,3 +136,30 @@ exports.readAll = function (req, res) {
         }
     });
 };
+
+exports.checkIndex = function (req, res) {
+
+    client.indices.exists({
+            index: "test-users"
+        }, (error, resp, status) => {
+            if (status == 404) {
+                client.indices.create({
+                    index: "test-users"
+                }, (error, resp, status) => {
+                    // if (error) {
+                    //     res.send("Error: " + error.message);
+                    // }
+                    // else {
+                    //     res.send("We have created new index!")
+                    // }
+
+                })
+            }
+            else {
+                // res.send("It already exists!")
+            }
+
+        }
+    )
+};
+
