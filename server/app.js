6f7f98c65b31647d83e9f4e57d15a1992c3b5ea9
LostSnaker <lostsnaker@gmail.com>
2019-08-02 15:43:55 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -11,9 +11,12 @@ var user = require('./routes/user_routes');
 app.use('/users', user);
 
 var port = process.env.PORT || 3000;
+var host = process.env.HOST || "localhost";
+var elasticPort = 9200;
 
 app.listen(port, function () {
     console.log('Example app listening on port ' + port);
-    user_controller.checkIndex();
+    user_controller.setElasticClient(host, elasticPort);
+    user_controller.checkIndex("test-users");
 });
 
--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -8,6 +8,5 @@ router.get('/:id', user_controller.userRead);
 router.put('/:id', user_controller.userUpdate);
 router.delete('/:id', user_controller.userDelete);
 router.get('/', user_controller.readAll);
-router.post('/check', user_controller.checkIndex);
 
 module.exports = router;
